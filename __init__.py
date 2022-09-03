ad7c28ffc20bb58f1c4457f08b43962f368ea5d2
Daniel McKnight <daniel@neon.ai>
2022-09-02 18:07:55 -0700

    Remove examples with parenthetical options from GUI display

--- __init__.py
+++ __init__.py
@@ -77,8 +77,9 @@ class AboutSkill(NeonSkill):
         """
         API Method to build a list of examples as listed in skill metadata.
         """
-        examples = [d.get('examples') or list() for d in self.skill_info]
-        flat_list = [item for sublist in examples for item in sublist]
+        examples = (d.get('examples') or list() for d in self.skill_info)
+        flat_list = [item for sublist in examples for item in sublist
+                     if not any((sym in item for sym in ('{', '/', '(', '[')))]
         return flat_list
 
     def _update_skills_data(self):
